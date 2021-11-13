31883dbb5609b6470c73720a09f81a37039c3f92
Alvaro Starling Hernandez Luzon <alvarostarling24@gmail.com>
2021-11-13 17:40:14 -0400

    Creando entidad Vacantes

---
+++ Entidades/Vacantes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_ContratacionEmpleados.Entidades
+{
+    public class Vacantes
+    {
+        public int VacanteId { get; set; }
+        public string NombreDeVacante { get; set; }
+        public int DepartamentoId { get; set; }
+        public DateTime FechaDeRegistro { get; set; } = DateTime.Now;
+        public string Requisitos { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
