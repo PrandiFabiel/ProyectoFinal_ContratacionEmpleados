629bcd6017d95a5c078cabe9b4da03aea6c201c3
Alvaro Starling Hernandez Luzon <alvarostarling24@gmail.com>
2021-11-20 22:35:32 -0400

    Agregando entidad Ciudades

---
+++ Entidades/Sectores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_ContratacionEmpleados.Entidades
+{
+    public class Sectores
+    {
+        [Key]
+        public int SectorId { get; set; }
+        public string Nombre { get; set; }
+        public int CiudadId { get; set; }
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("CiudadId")]
+        public virtual Ciudades Ciudades { get; set; }
+    }
+}
