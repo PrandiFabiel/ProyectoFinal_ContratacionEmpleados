39f6dc8b1dccbb7a8839cabcbe5f89c3145586ce
fabie <fabie@DESKTOP-ILTFSL4>
2021-11-21 19:43:53 -0400

    Se modifico el registro de permisos.

---
+++ Migrations/20211121233518_Volviendo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoFinal_ContratacionEmpleados.Migrations
+{
+    public partial class Volviendo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "UsuarioId",
+                keyValue: 1,
+                column: "FechaRegistroUsuario",
+                value: new DateTime(2021, 11, 21, 19, 35, 16, 861, DateTimeKind.Local).AddTicks(9345));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "UsuarioId",
+                keyValue: 1,
+                column: "FechaRegistroUsuario",
+                value: new DateTime(2021, 11, 21, 17, 3, 56, 346, DateTimeKind.Local).AddTicks(7047));
+        }
+    }
+}
