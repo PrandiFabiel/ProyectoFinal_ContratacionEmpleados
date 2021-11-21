921b220cfb24a22de47f1c6b9284ea9720a3000c
fabie <fabie@DESKTOP-ILTFSL4>
2021-11-21 16:46:54 -0400

    Se diseno y programo el login

---
+++ UI/Login.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ProyectoFinal_ContratacionEmpleados.BLL;
+using ProyectoFinal_ContratacionEmpleados.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal_ContratacionEmpleados.UI
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        Usuarios usuarios = new Usuarios();
+        MainWindow Principal = new MainWindow();
+
+
+        private void IngresarButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool paso = LoginBLL.Validar(NombreUsuarioTextBox.Text, ContrasenaPasswordBox.Password);
+
+            if (paso)
+            {
+                this.Close();
+                Principal.Show();
+            }
+            else
+            {
+                MessageBox.Show("Error Nombre Usuario o Contraseña incorrecta!", "Error!");
+                ContrasenaPasswordBox.Clear();
+                NombreUsuarioTextBox.Focus();
+            }
+        }
+
+        private void CancelarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
