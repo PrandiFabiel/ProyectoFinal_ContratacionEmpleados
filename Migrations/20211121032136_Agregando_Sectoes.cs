4f6ebb53aa4981cbf05373482221fe23c7a62cb8
Alvaro Starling Hernandez Luzon <alvarostarling24@gmail.com>
2021-11-20 23:36:33 -0400

    Agragndo registro de sectores y haciedo migracion

---
+++ Migrations/20211121032136_Agregando_Sectoes.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoFinal_ContratacionEmpleados.Migrations
+{
+    public partial class Agregando_Sectoes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sectores",
+                columns: table => new
+                {
+                    SectorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: true),
+                    CiudadId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UsuarioId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sectores", x => x.SectorId);
+                    table.ForeignKey(
+                        name: "FK_Sectores_Ciudades_CiudadId",
+                        column: x => x.CiudadId,
+                        principalTable: "Ciudades",
+                        principalColumn: "CiudadId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sectores_CiudadId",
+                table: "Sectores",
+                column: "CiudadId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sectores");
+        }
+    }
+}
---
+++ UI/Registros/rSectores.xaml.cs
@@ -0,0 +1,117 @@
+﻿using ProyectoFinal_ContratacionEmpleados.BLL;
+using ProyectoFinal_ContratacionEmpleados.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal_ContratacionEmpleados.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rSectores.xaml
+    /// </summary>
+    public partial class rSectores : Window
+    {
+        private Sectores sector = new Sectores();
+
+        public rSectores()
+        {
+            InitializeComponent();
+            this.DataContext = sector;
+
+            CiudadComboBox.ItemsSource = CiudadesBLL.GetCiudades();
+            CiudadComboBox.SelectedValuePath = "CiudadId";
+            CiudadComboBox.DisplayMemberPath = "Nombre";
+        }
+
+        private void Limpiar()
+        {
+            this.sector = new Sectores();
+            NombreTextBox.Text = "";
+            this.DataContext = sector;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (NombreTextBox.Text.Length == 0)
+            {
+                esValido = false;
+
+                MessageBox.Show("El campo Nombre está vacio", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                NombreTextBox.Focus();
+            }
+
+            if (SectoresBLL.ExisteNombre(NombreTextBox.Text) == true)
+            {
+                esValido = false;
+
+                MessageBox.Show("Ya existe un Sector con este nombre!", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                NombreTextBox.Focus();
+            }
+
+            return esValido;
+        }
+
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var sect = SectoresBLL.Buscar(Utilidades.ToInt(SectorIdTextBox.Text));
+
+            if (sector != null)
+                this.sector = sect;
+            else
+                this.sector = new Sectores();
+
+            this.DataContext = this.sector;
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validar())
+                return;
+
+            var paso = SectoresBLL.Guardar(sector);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Transaccion exitosa!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion Fallida!", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SectoresBLL.Eliminar(Utilidades.ToInt(SectorIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
