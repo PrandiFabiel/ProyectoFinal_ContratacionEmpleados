7b106c1dd524b417773e7bb855949296b7dde508
fabie <fabie@DESKTOP-ILTFSL4>
2021-11-21 16:31:30 -0400

    Subiendo bll de login

---
+++ BLL/LoginBLL.cs
@@ -0,0 +1,54 @@
+﻿using ProyectoFinal_ContratacionEmpleados.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_ContratacionEmpleados.BLL
+{
+    public class LoginBLL
+    {
+        public static bool Validar(string nombreusuario, string contrasena)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var validar = from usuario in contexto.Usuarios
+                              where usuario.NombreUsuario == nombreusuario
+                              && usuario.Clave == GetSHA256(contrasena)
+                              select usuario;
+
+                if (validar.Count() > 0)
+                    paso = true;
+                else
+                    paso = false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static string GetSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+    }
+}
